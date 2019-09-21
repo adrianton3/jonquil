0f7a74e7341d57a6afb451e7172e280789cf6f8a
Adrian Toncean <adrianton3@gmail.com>
2019-09-21 11:24:24 +0200

    Add background fade on interaction

--- src/main.js
+++ src/main.js
@@ -22,12 +22,29 @@
 		})
     }
 
-    function setupInteraction (canvas, plant) {
+    function setupInteraction (canvas, renderer, plant) {
+        let sign = -1.
+        let factor = 0.
+
         canvas.addEventListener('click', (event) => {
             event.preventDefault()
 
             plant.bareMode = !plant.bareMode
+            sign = -sign
         })
+
+        return {
+            tick () {
+                factor += sign * .007
+                factor = factor < 0 ? 0 : factor > 1 ? 1 : factor
+
+                renderer.setBackground(
+                    (1 - factor) * 255 + factor * 190,
+                    (1 - factor) * 255 + factor * 80,
+                    (1 - factor) * 255 + factor * 110,
+                )
+            }
+        }
     }
 
     function setup ({ images }) {
@@ -44,18 +61,20 @@
 
         plant.add(trunk)
 
-        setupInteraction(canvas, plant)
+        const interaction = setupInteraction(canvas, renderer, plant)
 
-        return { images, blobbery, plant, trunk }
+        return { images, blobbery, plant, trunk, interaction }
     }
 
-    function loop ({ plant, blobbery, trunk }) {
+    function loop ({ plant, blobbery, trunk, interaction }) {
 		let timePrevious = performance.now()
 
 		function step (timeNow) {
             const delta = (timeNow - timePrevious) * .001
             plant.tick(delta)
             blobbery.tick(delta)
+
+            interaction.tick(delta)
 
 			timePrevious = timeNow
 
